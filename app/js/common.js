159a99f3fd5408480d1211aaa9fb91de1f30f1ae
Dima Karelin <karelinds@mail.ru>
2018-02-02 17:21:14 +0400

    hover li

--- app/js/common.js
+++ app/js/common.js
@@ -3,31 +3,30 @@ $('document').ready(function() {
 	$('li.li__topmenu').click(function() {
 		$('li.li__topmenu').removeClass('active__menu--li')
 		$(this).addClass('active__menu--li');
-        $('.topmenu__products').css('display','none')
 	})
 
-    $('.li__topmenu a').click(function(e) {
-        e.preventDefault();
-     })
 
     $('li.li__topmenu--products').click(function() {
         $('.topmenu__products').css('display','block')
         $('.topmenu__products li a').css('color','white')
     })
 
-     $('li.li__topmenu--products').hover(function() {
-        $('.topmenu__products').css('display','block')
-        $('.topmenu__products li a').css('color','white')
-    })
-     $('li.li__topmenu--products').mouseleave(function() {
-        $('.topmenu__products').css('display','none')
-    })
-
-
-      $('.topmenu__products li a').hover(function() {
-        $('.topmenu__products li a').css('color','white')
-        $(this).css('color','#dc3236')
-    })
+       // Все для меню
+     //Отмена на нажатие
+   $('.li__topmenu').click(function(e) {
+         e.preventDefault();
+         return false;
+     });
+     //Конец Отмены на нажатие
+     //Открытие при наведении
+     $('.li__topmenu').hover(function() {
+         $(this).addClass('active__menu--li');
+         $(this).children('ul').slideDown();    
+     }, function() {
+         $(this).removeClass('active__menu--li');
+         $(this).children('ul').slideUp();
+     });
+     //Все для меню окончание , коментируй свой код
 
 	$('.pls__otkos').owlCarousel({
     loop: true,
