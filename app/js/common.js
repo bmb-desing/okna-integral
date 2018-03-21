7963329f45228caf15d4c17248848f268ae9dc45
xolms111 <35960774+xolms111@users.noreply.github.com>
2018-03-21 18:11:53 +0300

    123

--- app/js/common.js
+++ app/js/common.js
@@ -87,6 +87,23 @@
             }
         }).magnificPopup('open');
     });
+     $('.popup-block').magnificPopup({
+         delegate: 'a',
+         type: 'image',
+         tLoading: 'Loading image #%curr%...',
+         mainClass: 'mfp-img-mobile',
+         gallery: {
+             enabled: true,
+             navigateByImgClick: true,
+             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+         },
+         image: {
+             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+             titleSrc: function(item) {
+                 return item.el.attr('title');
+             }
+         }
+     });
 
     $('.index__owl').owlCarousel({
     loop: true,
@@ -107,8 +124,40 @@
         },
 
     }
+
 })
 
+
+var owl2 = $('.part');
+     owl2.owlCarousel({
+         loop: true,
+         nav: false,
+         dots: false,
+         //autoWidth: true,
+         //center: true,
+         autoplay: true,
+         //margin: 10,
+         responsive: {
+             0: {
+                 margin: 0,
+                 items: 1
+             },
+             600: {
+                 margin: 20,
+                 items: 2
+             },
+             1000: {
+                 margin: 30,
+                 items: 4
+             }
+         }
+     });
+     owl2.on('changed.owl.carousel', function(event) {
+         var items = $('.part .owl-item.active div');
+         var activeItem = items[0];
+         var title = activeItem.attributes[0].value;
+         $('.partners h6').text(title || 'Наши партнеры');
+     });
 var owl = $('.owl-carousel');
 $('.forward').click(function() {
     owl.trigger('next.owl.carousel');
@@ -124,89 +173,93 @@ $(".phone").mask("+ 9(999)999-9999");
 $(document).ready(function () {
     //Калькулятор
     //Переключение качества
-    $('.calc__check').click(function () {
-        if (!$(this).hasClass('active')) {
-            $('.calc__check').removeClass('active');
-            $(this).addClass('active');
-            var noise = $(this).attr('data-noise');
-            var protect = $(this).attr('data-protect');
-            var sun = $(this).attr('data-sun');
-            var energy = $(this).attr('data-energy');
-            editProgress(noise, 'noise');
-            editProgress(protect, 'protect');
-            editProgress(sun, 'sun');
-            editProgress(energy, 'energy');
-            activeWindow();
-        }
-    })
-    //Переключение изображений
-    $('.calc__lists li ul li').click(function () {
-        if(!$(this).hasClass('active')) {
-            $('.calc__lists li ul li').removeClass('active');
-            $('.calc__lists_item').removeClass('active');
-            var image = $(this).children('img').attr('data-image');
-            var window = $(this).parents('.calc__lists_item');
-            window.addClass('active');
-            $(this).addClass('active');
-            $('.calculator__image').attr('src', image);
-            activeWindow();
-        }
-    })
-    //Чекбоксы модификаций
-    $('.modify li').click(function () {
-       $(this).toggleClass('active');
-       var ind = $(this).index();
-       var price = calc[3][ind];
-       var mod = $(this).attr('data-modify');
-       if($(this).hasClass('active')) {
-           addModify(price, mod);
-       }
-       else {
-           removeModify(price, mod);
-       }
-    });
-    $('.button__calc').click(function(e) {
+    if ($('.calc__check').length) {
+        $('.calc__check').click(function () {
+            if (!$(this).hasClass('active')) {
+                $('.calc__check').removeClass('active');
+                $(this).addClass('active');
+                var noise = $(this).attr('data-noise');
+                var protect = $(this).attr('data-protect');
+                var sun = $(this).attr('data-sun');
+                var energy = $(this).attr('data-energy');
+                editProgress(noise, 'noise');
+                editProgress(protect, 'protect');
+                editProgress(sun, 'sun');
+                editProgress(energy, 'energy');
+                activeWindow();
+            }
+        })
+        //Переключение изображений
+        $('.calc__lists li ul li').click(function () {
+            if (!$(this).hasClass('active')) {
+                $('.calc__lists li ul li').removeClass('active');
+                $('.calc__lists_item').removeClass('active');
+                var image = $(this).children('img').attr('data-image');
+                var window = $(this).parents('.calc__lists_item');
+                window.addClass('active');
+                $(this).addClass('active');
+                $('.calculator__image').attr('src', image);
+                activeWindow();
+            }
+        })
+        //Чекбоксы модификаций
+        $('.modify li').click(function () {
+            $(this).toggleClass('active');
+            var ind = $(this).index();
+            var price = calc[3][ind];
+            var mod = $(this).attr('data-modify');
+            if ($(this).hasClass('active')) {
+                addModify(price, mod);
+            }
+            else {
+                removeModify(price, mod);
+            }
+        });
+        $('.button__calc').click(function (e) {
         e.preventDefault();
         $('.modal__calc').fadeIn();
         $('.modal__calc input[name=window]').val(callback_window);
         $('.modal__calc input[name=modify]').val(modifiers);
-    }) 
+    })
+    }
 });
-function editProgress(width, elem) {
-    $('.calculator__progress_'+elem).children('.progress').children('span').css('width', width+'%')
-}
-var price = 0;
-var modify = 0;
-var modifiers = [];
-var callback_window = 'Качество окна: 0 Вид окна: 0 Окно: 0';
-var active_window = calc[0][0][0]
-function pricing() {
-    var price = active_window;
-    var full_price = active_window+modify;
-    $('.price__item_empty .price__var span').text(price);
-    $('.price__item_full .price__var span').text(full_price);
-}
-function activeWindow() {
-    var tab = $('.calc__lists_item.active').index();
-    var window = $('.calc__lists li ul li.active').index();
-    var status = $('.calc__check.active').index();
-    active_window = calc[status][tab][window];
-    callback_window = 'Качество окна: '+status+' Вид окна: '+tab+' Окно: '+window; 
-    pricing();
-}
-function addModify(price, mod) {
-    modify+=price;
-    var sum = active_window + modify;
-    modifiers.push(mod);
-    $('.price__item_full .price__var span').text(sum);
-}
-function removeModify(price, mod) {
+ if ($('.calc__check').length) {
+    function editProgress(width, elem) {
+        $('.calculator__progress_'+elem).children('.progress').children('span').css('width', width+'%')
+    }
+    var price = 0;
+    var modify = 0;
+    var modifiers = [];
+    var callback_window = 'Качество окна: 0 Вид окна: 0 Окно: 0';
+    var active_window = calc[0][0][0]
+    function pricing() {
+        var price = active_window;
+        var full_price = active_window+modify;
+        $('.price__item_empty .price__var span').text(price);
+        $('.price__item_full .price__var span').text(full_price);
+    }
+    function activeWindow() {
+        var tab = $('.calc__lists_item.active').index();
+        var window = $('.calc__lists li ul li.active').index();
+        var status = $('.calc__check.active').index();
+        active_window = calc[status][tab][window];
+        callback_window = 'Качество окна: '+status+' Вид окна: '+tab+' Окно: '+window;
+        pricing();
+    }
+    function addModify(price, mod) {
+        modify+=price;
+        var sum = active_window + modify;
+        modifiers.push(mod);
+        $('.price__item_full .price__var span').text(sum);
+    }
+    function removeModify(price, mod) {
 
-    modify-=price;
-    var sum = active_window + modify;
-    var findMod = modifiers.indexOf(mod);
-    modifiers.splice(findMod, 1);
+        modify-=price;
+        var sum = active_window + modify;
+        var findMod = modifiers.indexOf(mod);
+        modifiers.splice(findMod, 1);
 
-    $('.price__item_full .price__var span').text(sum);
-}
-pricing()
+        $('.price__item_full .price__var span').text(sum);
+    }
+    pricing()
+ }
